a832c160648b1380e37edb25b6192f26e651e677
Yifat Amir <yifatamir@berkeley.edu>
2016-01-28 21:54:57 -0800

    submitting four2one

---
+++ four2one.py
@@ -0,0 +1,37 @@
+class Game:
+
+    def __init__(self, pos, prim = 0):
+        self.initial_position = pos
+        self.primitive = prim #losing initial position
+        
+def solve(game):
+    pos = game.initial_position
+    prim = game.primitive
+    if pos < prim:
+        return 'poorly set up game'
+    if pos == prim:
+        return 'lose'
+    else:
+        chart = ['lose' for x in range(pos - prim + 1)]
+        curr = 1
+        while curr <= pos - prim:
+            successors = [chart[curr - x] for x in generate_moves(curr, prim)]
+            if 'lose' in successors:
+                chart[curr] = 'win'
+            curr += 1
+        return chart[pos-prim]
+
+def generate_moves(pos, prim = 0):
+    if pos > prim + 1:
+        return [1, 2]
+    elif pos == prim + 1:
+        return [1]
+    else:
+        return []
+
+
+
+#initial position
+#generate moves
+#do moves
+#primitives
